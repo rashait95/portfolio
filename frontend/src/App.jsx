6eb71f5cabe370dd30b7667657ffb78d4a4c3960
luqman1it <luqman1it@gmail.com>
2024-04-28 17:47:32 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './Component/Home/Home'
+import About from './Component/About/About'
+import Project from './Component/Project/Project'
+import Contact from './Component/Contact/Contact'
+function App() {
+
+
+  return (
+    <>
+      <Home />
+      <About />
+      <Project />
+      <Contact />
+    </>
+  )
+}
+
+export default App
